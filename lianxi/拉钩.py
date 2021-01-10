36519a7774cea1705fee39a1159d88d9150322d4
yb1814881542 <1814881542@qq.com>
2021-01-10 19:01:29 +0800

    selenuim

---
+++ lianxi/拉钩.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/1/8 18:29
+# @Author  : YB
+# @File    : 拉钩.py
+# @Software: PyCharm
+
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.keys import Keys
+import time
+
+web = Chrome()
+web.get('https://www.lagou.com/')
+web.find_element_by_xpath('//*[@id="cboxClose"]').click()
+time.sleep(1)
+web.find_element_by_xpath('//*[@id="search_input"]').send_keys('python', Keys.ENTER)
+alst = web.find_elements_by_class_name('position_link')
+n=0
+for a in alst:
+    a.find_element_by_tag_name('h3').click()
+    web.switch_to_window(web.window_handles[-1])
+    text = web.find_element_by_xpath('//*[@id="job_detail"]/dd[2]').text
+    with open('%s.txt' % n, 'w')as fp:
+        fp.write(text)
+    web.close()
+    web.switch_to_window(web.window_handles[0])
+    time.sleep(1)
+    n += 1
